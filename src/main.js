214efffce257e09c7a1028642b328b9f53a0a90f
汪智鹏 <1848993253@qq.com>
2023-01-03 16:05:21 +0800

    引入iconfont文件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import '@/assets/iconfont/iconfont.css';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/styles/reset.css';
 import '@/styles/global.scss';
