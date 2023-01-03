b356a92d214c31e172709b73f085558cc1293e82
汪智鹏 <1848993253@qq.com>
2023-01-03 14:23:46 +0800

    toolbar组件样式

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="toolbar">
+    <el-button @click="console.log(111);">JSON</el-button>
+    <el-button @click="console.log(111);">撤消</el-button>
+    <el-button @click="console.log(111);">重做</el-button>
+    <label for="input" class="insert">
+        插入图片
+        <input
+            id="input"
+            type="file"
+            hidden
+            @change="console.log(111);"
+        />
+    </label>
+
+    <el-button style="margin-left: 10px;" @click="console.log(111);">预览</el-button>
+    <el-button @click="console.log(111);">保存</el-button>
+    <el-button @click="console.log(111);">清空画布</el-button>
+    <el-button @click="console.log(111);">组合</el-button>
+    <el-button
+        @click="console.log(111);"
+    >
+        拆分
+    </el-button>
+
+    <el-button @click="console.log(111);">锁定</el-button>
+    <el-button @click="console.log(111);">解锁</el-button>
+    <el-button @click="console.log(111);">截图</el-button>
+
+    <div class="canvas-config">
+        <span>画布大小</span>
+        <input v-model="canvasStyleData.width">
+        <span>*</span>
+        <input v-model="canvasStyleData.height">
+    </div>
+    <div class="canvas-config">
+        <span>画布比例</span>
+        <input v-model="scale" @input="handleScaleChange"> %
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scale: 100,
+      canvasStyleData: {
+        width: '100',
+        height: '100',
+      },
+    };
+  },
+  methods: {
+    handleScaleChange() {
+
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.toolbar {
+    padding: 15px 10px;
+    white-space: nowrap;
+    overflow-x: auto;
+    background: #fff;
+    border-bottom: 1px solid #ddd;
+
+    .canvas-config {
+        display: inline-block;
+        margin-left: 10px;
+        font-size: 14px;
+        color: #606266;
+
+        input {
+            width: 50px;
+            margin-left: 4px;
+            outline: none;
+            padding: 0 5px;
+            border: 1px solid #ddd;
+            color: #606266;
+        }
+
+        span {
+            margin-left: 10px;
+        }
+    }
+
+    .insert {
+        display: inline-block;
+        line-height: 1;
+        white-space: nowrap;
+        cursor: pointer;
+        background: #fff;
+        border: 1px solid #dcdfe6;
+        color: #606266;
+        appearance: none;
+        text-align: center;
+        box-sizing: border-box;
+        outline: 0;
+        margin: 0;
+        transition: .1s;
+        font-weight: 500;
+        padding: 9px 15px;
+        font-size: 12px;
+        border-radius: 3px;
+        margin-left: 10px;
+
+        &:active {
+            color: #3a8ee6;
+            border-color: #3a8ee6;
+            outline: 0;
+        }
+
+        &:hover {
+            background-color: #ecf5ff;
+            color: #3a8ee6;
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,76 @@
 <template>
   <div class="home">
-    Home
+    <Toolbar />
   </div>
 </template>
 
-<script></script>
+<script>
+import Toolbar from '@/components/Toolbar.vue';
 
-<style lang="scss"></style>
+export default {
+  components: { Toolbar },
+};
+</script>
+
+<style lang="scss">
+.home {
+    height: 100vh;
+    background: #fff;
+
+    main {
+        height: calc(100% - 64px);
+        position: relative;
+
+        .left {
+            position: absolute;
+            height: 100%;
+            width: 200px;
+            left: 0;
+            top: 0;
+
+            & > div {
+                overflow: auto;
+
+                &:first-child {
+                    border-bottom: 1px solid #ddd;
+                }
+            }
+        }
+
+        .right {
+            position: absolute;
+            height: 100%;
+            width: 288px;
+            right: 0;
+            top: 0;
+            .el-select {
+                width: 100%;
+            }
+        }
+
+        .center {
+            margin-left: 200px;
+            margin-right: 288px;
+            background: #f5f5f5;
+            height: 100%;
+            overflow: auto;
+            padding: 20px;
+
+            .content {
+                width: 100%;
+                height: 100%;
+                overflow: auto;
+            }
+        }
+    }
+
+    .placeholder {
+        text-align: center;
+        color: #333;
+    }
+
+    .global-attr {
+        padding: 10px;
+    }
+}
+</style>
